97cbfbc650fdbe90b9abe6397799f497c0ec4564
unknown <mayureshsaitwal25@gmail.com>
2024-05-09 03:20:18 +0530

    Top of Input Page

--- pages/input_page.dart
+++ pages/input_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable, recursive_getters
+// ignore_for_file: must_be_immutable, recursive_getters, prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -12,16 +12,69 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  get _ifcode => widget.ifcode;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("InputPage"),
-          centerTitle: true,
-          backgroundColor: Colors.blue[400],
-          titleTextStyle: const TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black),
+      appBar: AppBar(
+        title: const Text(
+          "India Forge and Drop Stampings Ltd.",
+          style: TextStyle(fontSize: 18),
         ),
-        body: Center(child: Text(widget.ifcode)));
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+        titleTextStyle: const TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black),
+      ),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  child: Center(
+                      child: const Text("Inprocess Inspection : X-R Chart")),
+                  color: Colors.lightBlue.shade100,
+                  width: MediaQuery.of(context).size.width),
+              Container(
+                  color: Colors.grey.shade300,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Row(
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("IF CODE: $_ifcode"),
+                          Text("Part Name: "),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Dimension: "),
+                          Text("UCL: "),
+                          Text("LCL: "),
+                        ],
+                      ),
+                    ],
+                  )),
+              Container(
+                  color: Colors.yellow.shade100,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Machine No: "),
+                      Text("Operator Name: "),
+                    ],
+                  )),
+            ],
+          ),
+          // ListView.builder(itemBuilder: itemBuilder)
+        ],
+      ),
+    );
   }
 }
